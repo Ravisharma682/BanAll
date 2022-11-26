1b761a2b8accae40477d0325a100738ac2af3ab4
Ravi Sharma <118602885+Ravisharma682@users.noreply.github.com>
2022-11-26 20:20:26 +0530

    Add files via upload

--- bot/config.py
+++ bot/config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config:
-    TELEGRAM_TOKEN=os.environ['TELEGRAM_TOKEN']
-    SUDOS=os.environ['SUDOS']
-    TELEGRAM_APP_HASH=os.environ['TELEGRAM_APP_HASH']
-    TELEGRAM_APP_ID=int(os.environ['TELEGRAM_APP_ID'])
+    TELEGRAM_TOKEN=os.environ["TELEGRAM_TOKEN", "5919847405:AAEASoQD1KnDBuPGr60xa8GUhUWjAUlIAPk"]
+    SUDOS=os.environ["SUDOS", "1964732367"]
+    TELEGRAM_APP_HASH=os.environ["TELEGRAM_APP_HASH", "1c40c54693e2cdbe51f90a152ed1bd5f"]
+    TELEGRAM_APP_ID=int(os.environ["TELEGRAM_APP_ID", "5994204"])
     
     if not TELEGRAM_TOKEN:
-        raise ValueError('TELEGRAM BOT TOKEN not set')
+        raise ValueError("TELEGRAM BOT TOKEN not set")
     
     if not TELEGRAM_APP_HASH:
         raise ValueError("TELEGRAM_APP_HASH not set")
